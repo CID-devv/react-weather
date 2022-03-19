5201245cc52ca2352f9dc327252ce6f590cea12d
Caleb Dauphin <calebdauphindev@gmail.com>
2022-03-19 13:34:07 -0400

    add bootstrap and add container div in app.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className='App'>
-			<h1>Weather App</h1>
-			<footer>
-				<p>
-					This project was coded by{" "}
-					<a
-						href='http://www.cassandradauphin.com'
-						target='_blank'
-						rel='noreferrer noopener'>
-						Cassandra Dauphin{" "}
-					</a>
-					and is{" "}
-					<a
-						href='https://github.com/CalebDauphin/react-weather'
-						target='_blank'
-						rel='noreferrer noopener'>
-						open sourced
-					</a>
-				</p>
-			</footer>
+			<div className='container'>
+				<h1>Weather App</h1>
+				<footer>
+					<p>
+						This project was coded by{" "}
+						<a
+							href='http://www.cassandradauphin.com'
+							target='_blank'
+							rel='noreferrer noopener'>
+							Cassandra Dauphin{" "}
+						</a>
+						and is{" "}
+						<a
+							href='https://github.com/CalebDauphin/react-weather'
+							target='_blank'
+							rel='noreferrer noopener'>
+							open sourced
+						</a>
+					</p>
+				</footer>
+			</div>
 		</div>
 	);
 }
