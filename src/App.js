1f44cb8f9e3105b12c32acb506a95c93a712a164
Caleb Dauphin <calebdauphindev@gmail.com>
2022-03-19 08:33:54 -0400

    Add github link to footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
-function App() {
-	return <div className='App'>Weather App</div>;
+export default function App() {
+	return (
+		<div className='App'>
+			<h1>Weather App</h1>
+			<footer>
+				<p>
+					This project was coded by{" "}
+					<a
+						href='http://www.cassandradauphin.com'
+						target='_blank'
+						rel='noreferrer noopener'>
+						Cassandra Dauphin{" "}
+					</a>
+					and is{" "}
+					<a
+						href='https://github.com/CalebDauphin/react-weather'
+						target='_blank'
+						rel='noreferrer noopener'>
+						open sourced
+					</a>
+				</p>
+			</footer>
+		</div>
+	);
 }
-
-export default App;
